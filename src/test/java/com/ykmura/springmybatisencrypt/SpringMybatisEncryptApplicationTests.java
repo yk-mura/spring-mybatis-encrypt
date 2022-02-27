1ad5c17c12e95428b784e4f51ce8b0c27760418f
YK <31270461+yk-mura@users.noreply.github.com>
2022-02-27 22:57:53 +0900

    Spring Initializerで初期化 (#1)
    * Spring Initializrで初期化
    
    * H2 Databaseの設定を追加
    
    Co-authored-by: y.nakamura <y.nakamura@linecorp.com>

---
+++ src/test/java/com/ykmura/springmybatisencrypt/SpringMybatisEncryptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ykmura.springmybatisencrypt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMybatisEncryptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
