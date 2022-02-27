7e38fc208deeedb39a73afd61f043467a9b38a21
y.nakamura <y.nakamura@linecorp.com>
2022-02-27 22:47:59 +0900

    Spring Initializrで初期化

---
+++ src/main/java/com/ykmura/springmybatisencrypt/SpringMybatisEncryptApplication.java
@@ -0,0 +1,13 @@
+package com.ykmura.springmybatisencrypt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMybatisEncryptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMybatisEncryptApplication.class, args);
+	}
+
+}
